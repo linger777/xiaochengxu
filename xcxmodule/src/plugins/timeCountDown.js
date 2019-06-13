eeb7099b6ee66e859a018f0cd8823a9ab5ad9cd3
Grace_QIN <linger_56688@163.com>
2019-06-13 17:02:56 +0800

    增加 倒计时 开始结束属性

--- xcxmodule/src/plugins/timeCountDown.js
+++ xcxmodule/src/plugins/timeCountDown.js
@@ -4,6 +4,8 @@ export default class TimeCD {
     // 默认配置
     self.conf = Object.assign({
       lastTime: 0, //剩余时间戳（s）
+      startTime: 0, // 开始时间戳
+      endTime: 0, // 结束时间戳
       format(val){
         return val < 10 ? '0'+val : val
       },    // 对 this.days,...,this.seconds 的值格式化
@@ -14,7 +16,7 @@ export default class TimeCD {
       onSecondChange(){}, // 秒数变化时回调
       onFinish(){},       // 倒计时结束回调
     }, conf)
-    self.lastTime = conf.lastTime
+    self.lastTime = conf.lastTime ? conf.lastTime : Math.floor(( conf.endTime - conf.startTime )/1000)
     self.ticks = 0
     self.countDown()
   }
